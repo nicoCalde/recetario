08f373cc68579addf396bfced47908e091160802
nicoCalde <nicogonzalezcalderon@gmail.com>
2022-10-25 12:59:05 -0300

    db inicializada

--- recetario/settings.py
+++ recetario/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'recetario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'recetario', 
+        'USER': 'postgres', 
+        'PASSWORD': '207',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
