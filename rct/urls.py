ad4280373b08fe6c6b48bd8cb5c4bcaabaec420b
nicoCalde <nicogonzalezcalderon@gmail.com>
2022-12-05 20:38:16 -0300

    app urls + templates + views(basicas)

---
+++ rct/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('mis_recetas',views.mis_recetas,name='mis_recetas'),
+    path('contact',views.contact,name='contact'),
+]
--- rct/views.py
+++ rct/views.py
@@ -1,3 +1,13 @@
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # listado_recetas =
+    return render(request,'rct/public/index.html',)#'recetas':listado_recetas )
+
+def mis_recetas(request):
+    return render(request,'rct/public/mis_recetas.html',)
+
+def contact(request):
+    return render(request,'rct/public/contact.html',)
