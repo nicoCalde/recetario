2a1db4ea0e3b92e9aaa3b841b382df3455b84a53
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-01-26 21:16:37 -0300

    variables de entorno listas para settear para prod

--- recetario/settings.py
+++ recetario/settings.py
@@ -22,12 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zuud_#$mck311=t%^!e=8dgo(tvcxjqh*y=fzxezb-erqy*gp2'
+# for production: SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-zuud_#$mck311=t%^!e=8dgo(tvcxjqh*y=fzxezb-erqy*gp2')
+# generate secret key command (terminal): python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# for production: DEBUG = str(os.environ.get('DEBUG')) == '1'
+# for production: reventalo, que ni este.
 
 ALLOWED_HOSTS = []
-
+#  for production:
+# if not DEBUG:
+#     ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 
