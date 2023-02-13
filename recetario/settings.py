904fbedd158f9c1a983ba9b8bbe0b9e30f0620c1
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-02-13 16:03:04 -0300

    DB .env configured

--- recetario/settings.py
+++ recetario/settings.py
@@ -90,9 +90,9 @@ WSGI_APPLICATION = 'recetario.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'recetas', 
-        'USER': 'postgres', 
-        'PASSWORD': '207',
+        'NAME': os.environ.get('DB_NAME'), 
+        'USER': os.environ.get('DB_USER'), 
+        'PASSWORD': os.environ.get('DB_USER'),
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
