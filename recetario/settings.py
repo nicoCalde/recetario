85e3a5150959498f0eda4708eef34c5b0de3c8a7
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-03-09 19:34:44 -0300

    new db

--- recetario/settings.py
+++ recetario/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'recetario.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'recetas', 
+        'NAME': 'recetarios', 
         'USER': 'postgres', 
         'PASSWORD': '207',
         'HOST': '127.0.0.1', 
