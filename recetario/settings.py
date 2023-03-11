120348cc5617a355ca36c7c0c90efd93a0f37359
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-03-11 16:26:20 -0300

    mysql configured

--- recetario/settings.py
+++ recetario/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'recetario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'), 
         'USER': os.environ.get('DB_USER'), 
         'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'HOST': os.environ.get('DB_HOST'), 
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
